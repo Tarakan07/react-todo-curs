7776e8137326ab4aa3bede96fd11583416af8918
Boris <boris_galunga@mail.ru>
2022-07-16 00:34:48 +0300

    create dataState

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -29,7 +29,7 @@ export default class TodoListItem extends Component {
 	//   };
 	// }
 	render() {
-		const { label } = this.props;
+		const { label, onDeleted } = this.props;
 
 		const { done, important } = this.state;
 		let classNames = "todo-list-item";
@@ -57,6 +57,7 @@ export default class TodoListItem extends Component {
 				<button
 					type="button"
 					className="btn btn-outline-danger btn-sm float-right"
+					onClick={onDeleted}
 				>
 					<i className="fa fa-trash-o" />
 				</button>
