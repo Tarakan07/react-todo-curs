1dc8ab8c02efab41912300818f4340847c76deb4
Boris <boris_galunga@mail.ru>
2022-07-15 21:07:02 +0300

    start commit lessons react todo

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./search-panel.css";
+
+const SearchPanel = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="type to search"
+      />
+    </div>
+  );
+};
+
+export default SearchPanel;
