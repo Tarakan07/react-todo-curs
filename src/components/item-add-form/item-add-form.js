a30a0238d349d3501b9909dc31ac8995bc59ef9a
Boris <boris_galunga@mail.ru>
2022-07-18 00:21:23 +0300

    work with form + create func for add new item

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,14 +2,32 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+	state = {
+		label: "",
+	};
+	onLabelChange = (e) => {
+		this.setState({
+			label: e.target.value,
+		});
+	};
+	onSubmit = (e) => {
+		e.preventDefault()``;
+		this.props.addItem(this.state.label);
+	};
 	render() {
 		const addItem = this.props.addItem;
 		return (
-			<div className="item-add-form">
-				<button onClick={() => this.props.addItem("klalala")}>
+			<form className="item-add-form" onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					className="form-control"
+					onChange={this.onLabelChange}
+					placeholder="Enter you item"
+				/>
+				<button>
 					<span>Add Item</span>
 				</button>
-			</div>
+			</form>
 		);
 	}
 }
